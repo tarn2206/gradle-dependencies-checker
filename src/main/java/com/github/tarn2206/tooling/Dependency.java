6e78380d3f10b9255af4abadf17a3adad8f163a6
t@rn <tarn.net@gmail.com>
2020-06-04 09:21:57 +0700

    update how to detect latest version

--- src/main/java/com/github/tarn2206/tooling/Dependency.java
+++ src/main/java/com/github/tarn2206/tooling/Dependency.java
@@ -1,5 +1,7 @@
 package com.github.tarn2206.tooling;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Dependency
@@ -37,14 +39,43 @@ public class Dependency
         return Objects.hash(group, name, version);
     }
 
-    public boolean hasLatestVersion()
-    {
-        return latestVersion != null && (latestVersion.length() != version.length() || latestVersion.compareTo(version) > 0);
-    }
-
     @Override
     public String toString()
     {
         return version == null ? group + ':' + name : group + ':' + name + ':' + version;
+    }
+
+    public boolean hasLatestVersion()
+    {
+        if (latestVersion == null) return false;
+
+        List<Integer> current = extractVersion(version);
+        List<Integer> latest = extractVersion(latestVersion);
+        int min = Math.min(current.size(), latest.size());
+        for (int i = 0; i < min; i++)
+        {
+            if (latest.get(i).equals(current.get(i))) continue;
+            return latest.get(i) > current.get(i);
+        }
+        return false;
+    }
+
+    private List<Integer> extractVersion(String version)
+    {
+        List<Integer> list = new ArrayList<>();
+        String[] a = version.split("\\.");
+        for (String s : a)
+        {
+            try
+            {
+                int n = Integer.parseInt(s);
+                list.add(n);
+            }
+            catch (Exception e)
+            {
+                break;
+            }
+        }
+        return list;
     }
 }
